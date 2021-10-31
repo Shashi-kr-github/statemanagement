cda8c17bdd03f188bcf7e4b086a136d7a22d06e8
Shashijass <shashi16ic14@gmail.com>
2021-10-31 15:47:43 +0530

    state management

---
+++ Statemangement/driver-code/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+//import {Counter} from './components/Counter';
+import {useEffect, useState} from "react";
+import {Todo} from './components/Todo'
+//const Wrapper = styled.div`
+//
+import Forms from './components/Form';
+
+
+function App() {
+ return (
+   <div className="App">
+    <Todo/>
+   </div>
+ );
+  
+}
+
+export default App;    
